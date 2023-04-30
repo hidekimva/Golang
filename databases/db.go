68702d2e25ceccf6c63362878cc770896ca6e687
Guilherme Hideki Noce <hidekinoce@gmail.com>
2023-04-30 18:54:25 -0300

    feat: update databases

---
+++ databases/db.go
@@ -0,0 +1,30 @@
+package databases
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hidekimva/golang/databases/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConectaBD(host string, user string, password string, dbname string, port int) {
+	stringDeConexao := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", host, user, password, dbname, port)
+
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+
+	if err != nil {
+		log.Panic("Failed connecting to database")
+	}
+
+	log.Println("Success connecting to database")
+
+	DB.AutoMigrate(&models.User{})
+
+}
