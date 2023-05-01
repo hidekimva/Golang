70009a08f45ed2f51a89526f6571db5b5f0f9c94
Guilherme Hideki Noce <hidekinoce@gmail.com>
2023-05-01 12:55:52 -0300

    feat: update validator user

--- databases/models/user.go
+++ databases/models/user.go
@@ -2,19 +2,27 @@ package models
 
 import (
 	"github.com/google/uuid"
+	"gopkg.in/validator.v2"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	ID       string `gorm:"primary_key"`
-	Name     string
-	UserName string
-	Email    string
-	Password string
+	ID       string `gorm:"primary_key" json:"id"`
+	Name     string `json:"name" validate:"nonzero, regexp=^[a-zA-Z]*$"`
+	UserName string `json:"username" validate:"min=4,max=40,regexp=^[a-zA-Z]*$"`
+	Email    string `json:"email" validate:"email"`
+	Password string `json:"password" validate:"nonzero, min=8"`
 }
 
 func (user *User) BeforeCreate(scope *gorm.DB) error {
 	user.ID = uuid.NewString()
+	return nil
+}
+
+func ValidUser(user *User) error {
+	if err := validator.Validate(user); err != nil {
+		return err
+	}
 	return nil
 }
